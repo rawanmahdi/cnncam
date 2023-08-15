9a3049c0d5e2009e9e4ae3780ca98e91559fbac0
rawanmahdi <mahdir3@mcmaster.ca>
2023-08-15 10:32:49 -0400

    update setup to match new hierarchy

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
-from setuptools import setup
-with open("README.md", 'r') as f:
+from setuptools import setup, find_packages
+with open("lib/README.md", 'r') as f:
     long_description = f.read()
 
 setup(
-    name="cnn-cam",
+    name="cnncam",
+    package_dir={'':'lib'},
+    packages=find_packages(where='lib'),
+    # packages=['cnncam'],
     long_description=long_description, 
     long_description_content_type = 'text/markdown',
-    version='0.0.1',
+    version='0.0.2',
     description='Gradient Based Class Activation Maps for TensorFlow models.',
-    py_modules=["tensorflow", "numpy"],
-    package_dir={'': 'cnn_cam'},
+    py_modules=["cnncam"],
+    install_requires= ["tensorflow>=2.0.0", 'numpy>=1.23'],
     extras_require = {
         "dev" : ["twine>=4.0.2"]
     }
